d02856197d4377c64e30482a46b747b4ac70c170
coffee_h4ck3r <jeunesseBurce@users.noreply.github.com>
2026-03-25 03:01:54 +0800

    Add more initial commit and storybook

---
+++ src/stories/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Button } from './Button';
+import BookipiLogo from '../assets/bookipi-logo.png';
+import './header.css';
+
+type User = {
+  name: string;
+};
+
+export interface HeaderProps {
+  user?: User;
+  onLogin?: () => void;
+  onLogout?: () => void;
+  onCreateAccount?: () => void;
+}
+
+export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
+  <header>
+    <div className="storybook-header">
+      <div>
+        <img src={BookipiLogo} width="120" height="30" />
+        <h1>Quiz Builder</h1>
+      </div>
+      <div>
+        {user ? (
+          <>
+            <span className="welcome">
+              Welcome, <b>{user.name}</b>!
+            </span>
+            <Button size="small" onClick={onLogout} label="Log out" />
+          </>
+        ) : (
+          <>
+            <Button size="small" onClick={onLogin} label="Log in" />
+            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+          </>
+        )}
+      </div>
+    </div>
+  </header>
+);
